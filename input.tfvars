2d0d7d137a5974fc22a3ad738553b449486c766a
Eric Melo <ericmelomp@gmail.com>
2022-08-13 23:56:44 -0300

    Criação arquivos (Vazios)

---
+++ input.tfvars
@@ -0,0 +1,2 @@
+#Default region us-east-1
+provider_region = "us-east-1"
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "provider_region" {
+  description = "Input the region of the resources which will be created"
+  type = string
+}
